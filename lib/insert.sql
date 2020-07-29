1e9f54360845e579040f91ba1ed61e6d886ed696
Tomas Perez <tomasitopg6@gmail.com>
2020-07-29 18:11:10 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 42, "male", "brown", "sweet", TRUE);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 25, "male", "blue", "reckless", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 10, "female", "black", "calm", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 69, "male", "brown", "stubborn", false);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 5, "female", "yellow", "cuddly", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 21, "male", "green", "grumpy", false);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 17, "female", "purple", "hungry", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, "female", "orange", "violent", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 25, "male", "blue", "reckless", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 10, "female", "black", "calm", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 69, "male", "brown", "stubborn", FALSE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 5, "female", "yellow", "cuddly", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 21, "male", "green", "grumpy", FALSE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 17, "female", "purple", "hungry", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, "female", "orange", "violent", FALSE);
