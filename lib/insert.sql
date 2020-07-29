9c4dd5f90a1076bfd1c20f0e211df9d97177a0ba
Tomas Perez <tomasitopg6@gmail.com>
2020-07-29 18:09:16 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,3 +4,4 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabith
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 69, "male", "brown", "stubborn", false);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 5, "female", "yellow", "cuddly", true);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 21, "male", "green", "grumpy", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 17, "female", "purple", "hungry", true);
