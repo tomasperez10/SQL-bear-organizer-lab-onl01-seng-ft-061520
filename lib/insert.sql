180b3e218b7202f4b5fe9b4b3692398aaa81bedf
Tomas Perez <tomasitopg6@gmail.com>
2020-07-29 18:06:08 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,3 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 42, "male", "brown", "sweet", true)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 42, "male", "brown", "sweet", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 25, "male", "blue", "reckless", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
